1cc72556f91243a289aba251d473e0f09b664cfb
unknown <umeshbilagi@gmail.com>
2023-05-11 08:27:01 +0530

    done

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,3 @@
+import { AUTH_TOKEN, authToken } from "./routes/authToken";
+export { AUTH_TOKEN, authToken };
 //# sourceMappingURL=index.d.ts.map
---
+++ lib/routes/authToken.d.ts
@@ -0,0 +1,5 @@
+export interface AUTH_TOKEN {
+    token: string;
+}
+export declare const authToken: (clientId: string, clientSecrete: string, url: string) => Promise<AUTH_TOKEN>;
+//# sourceMappingURL=authToken.d.ts.map
